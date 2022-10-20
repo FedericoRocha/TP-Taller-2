9c70b9a233b536586f8527cbb45ddcb646dd6c64
Gabriel Halcague <42984507+GabrielHalcague@users.noreply.github.com>
2022-10-19 21:24:01 -0300

    elimine el carrusel

--- src/app/componentes/componentes-especificos/carrousel-cards/carrousel-cards.component.ts
+++ src/app/componentes/componentes-especificos/carrousel-cards/carrousel-cards.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductoComponent } from '../producto/producto.component';
 
 @Component({
   selector: 'app-carrousel-cards',
@@ -6,7 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carrousel-cards.component.css']
 })
 export class CarrouselCardsComponent implements OnInit {
-
+  
+  productos=
+  [{  
+    imagen1:"./assets/zapatilla1.webp",
+    linkcompra:"productoDescripcion",
+  nombre:'ZAPATILLAS ADIDAS ORIGINALS NITEBALL 2.0',
+  precio:'35.999',
+  cantidad: '',
+  color:'negra',
+  ano:'2022',    },
+  {  
+    imagen1:"./assets/zapatilla2.webp",
+    linkcompra:"productoDescripcion",
+    nombre:'ZAPATILLAS PUMA TRC BLAZE',
+  precio:'29.999',
+  cantidad: '',
+  color:'negra',
+  ano:'2022',    },
+  {  
+    imagen1:"./assets/zapatilla3.webp",
+    linkcompra:"productoDescripcion",
+    nombre:'ZAPATILLAS FILA EXCURSION',
+  precio:'35.999',
+  cantidad: '',
+  color:'negra',
+  ano:'2022',    },
+  {  
+    imagen1:"./assets/zapatilla4.webp",
+    linkcompra:"productoDescripcion",
+    nombre:'ZAPATILLAS FILA RENNO',
+  precio:'$25.489',
+  cantidad: '',
+  color:'negra',
+  ano:'2022',    },
+]
   constructor() { }
 
   ngOnInit(): void {
--- src/app/componentes/componentes-especificos/producto/producto.component.ts
+++ src/app/componentes/componentes-especificos/producto/producto.component.ts
@@ -5,8 +5,18 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './producto.component.html',
   styleUrls: ['./producto.component.scss']
 })
-export class ProductoComponent implements OnInit {
-
+export class ProductoComponent implements OnInit { 
+    id='';
+    nombre='';
+    precio='';
+    cantidad='';
+    color='';
+    ano='';
+    marca='';
+    linkcompra="producto-descripcion";
+    imagen1=''; 
+    imagen2=''; 
+    imagen3=''; 
   constructor() { }
 
   ngOnInit(): void {
