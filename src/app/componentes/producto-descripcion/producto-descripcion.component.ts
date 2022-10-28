4b352768499ebc0529d39616f80c67a86c28b91c
Gabriel Halcague <42984507+GabrielHalcague@users.noreply.github.com>
2022-10-28 00:25:34 -0300

    carrito terminado

--- src/app/componentes/producto-descripcion/producto-descripcion.component.ts
+++ src/app/componentes/producto-descripcion/producto-descripcion.component.ts
@@ -2,7 +2,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Talles } from 'src/app/interfaces/talles';
-import { Zapatilla } from 'src/app/interfaces/zapatilla';
 
 
 import { ZapatillaDescripcion } from 'src/app/interfaces/zapatillaDescripcion';
@@ -16,7 +15,8 @@ import { ZapatillaCarrito } from 'src/app/interfaces/zapatillaCarrito';
 })
 export class ProductoDescripcionComponent implements OnInit {
 
-  zapatillaCarrito:ZapatillaCarrito;
+
+
   // aca tengo un problema, zapattila carrito tiene que ser una convinacion de zapatila descripcion y un valor del talle, nose como mandarlo  
 
   zapatillaDescripcion:ZapatillaDescripcion ={
@@ -35,16 +35,40 @@ export class ProductoDescripcionComponent implements OnInit {
     material:"SINTETICO",
     descripcionProducto:"• Construida sobre una base de alto rendimiento para brindar máxima sujeción y comodidad. Con una suela exterior duradera que proporciona un agarre prolongado en superficies mojadas Un antepié más ancho proporciona el ajuste cómodo que necesita para recorridos más largos.",
   }
-  talles:Talles[]=[{ numero:35,stock:10 },{ numero:37,stock:1 },{ numero:40,stock:5 }
-]
+
+  talles:Talles[]=[{ numero:35,stock:10 },{ numero:37,stock:1 },{ numero:40,stock:5 }]
+  //talle:Talles;
+  talle:number;
+
+ 
+ 
 
   constructor(
     private route: ActivatedRoute,
     private cartService: CartService
   ) { 
   }
-  addToCart(product: ZapatillaCarrito) {
-    this.cartService.addToCart(product);
+
+  agregarCarrito(zapatillaDescripcion: ZapatillaDescripcion) {
+
+    let talles2 = {} as Talles;
+    for(var i=0; i<this.talles.length;i++){
+      if(this.talles[i].numero==this.talle){
+        talles2=this.talles[i];
+      }
+    }
+
+    let zapatillaCarrito = {} as ZapatillaCarrito;
+   zapatillaCarrito.id=zapatillaDescripcion.id;
+    zapatillaCarrito.nombre=zapatillaDescripcion.nombre;
+    zapatillaCarrito.imagen1=zapatillaDescripcion.imagen1;
+    zapatillaCarrito.precio=zapatillaDescripcion.precio;
+    zapatillaCarrito.color=zapatillaDescripcion.color;
+    zapatillaCarrito.marca=zapatillaDescripcion.marca;  
+    zapatillaCarrito.talle=talles2.numero;
+    zapatillaCarrito.stock=talles2.stock;
+    zapatillaCarrito.cantidad=1;
+    this.cartService.addToCart(zapatillaCarrito);
     window.alert('Your product has been added to the cart!');
   }
 
